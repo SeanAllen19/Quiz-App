990bfcef5350f4f14b32f6a744ece90b221397cf
SeanAllen19 <saallen19.dev@gmail.com>
2023-01-05 17:02:08 -0600

    created timer

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,22 @@
-console.log('is this thing on?')
+var timeHeader = document.querySelector('h3')
+var timeSecond = 5;
+displayTime(timeSecond)
+
+var countDown = setInterval( () => {
+    timeSecond--;
+    displayTime(timeSecond);
+    if(timeSecond < 0){
+        endTime()
+        clearInterval(countDown)
+    }
+}, 1000)
+
+function displayTime(second) {
+    var min = Math.floor(second / 60);
+    var sec = Math.floor(second % 60);
+    timeHeader.innerHTML = `${min<10 ? '0': ''}${min}:${sec<10? '0' : ''}${sec}`;
+}
+
+function endTime() {
+    timeHeader.innerHTML = 'Something new'
+}
