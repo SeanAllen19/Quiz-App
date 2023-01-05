0a78dd62408ab278252bb95dc96256e88e566260
SeanAllen19 <saallen19.dev@gmail.com>
2023-01-05 17:54:29 -0600

    updated timers on other pages and created question var

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,5 +1,6 @@
+// Timer Section
 var timeHeader = document.querySelector('h3')
-var timeSecond = 5;
+var timeSecond = 60;
 displayTime(timeSecond)
 
 var countDown = setInterval( () => {
@@ -20,3 +21,111 @@ function displayTime(second) {
 function endTime() {
     timeHeader.innerHTML = 'Something new'
 }
+
+
+// Questions
+var question = [
+    {
+        question: "Who is the main character in Pokemon?" ,
+        choice1: "Ash",
+        choice2: "Misty",
+        choice3: "Brock",
+        choice4: "Mewtwo",
+        answer: 1
+
+    },
+
+    {
+        question: "Which of these Pokemon types don't exist?" ,
+        choice1: "Rock",
+        choice2: "Poison",
+        choice3: "Glass",
+        choice4: "Metal",
+        answer: 3
+
+    }, 
+
+    {
+        question: "How many Eevee evolutions are there?" ,
+        choice1: "4",
+        choice2: "6",
+        choice3: "8",
+        choice4: "10",
+        answer: 3
+
+    },
+    {
+        question: "Who is Pokemon number 1 in the Pokedex?",
+        choice1: "Charmander",
+        choice2: "Bulbasaur",
+        choice3: "Squirtle",
+        choice4: "Pikachu",
+        answer: 2
+
+    },
+    {
+        question: "How many Pokemon are there?" ,
+        choice1: "151",
+        choice2: "913",
+        choice3: "989",
+        choice4: "1000+, Who cares?",
+        answer: 2
+    },
+    {
+        question: "How many Pokeballs are there?" ,
+        choice1: "2",
+        choice2: "7",
+        choice3: "13",
+        choice4: "27",
+        answer: 4
+
+    },
+    {
+        question: "Where does Pokemon originate from?" ,
+        choice1: "Japan",
+        choice2: "China",
+        choice3: "America",
+        choice4: "A Computer",
+        answer: 1
+
+    },
+    {
+        question: "What's Ash's last name?" ,
+        choice1: "Ketchup",
+        choice2: "Kaughthem",
+        choice3: "Ketchum",
+        choice4: "Kepchup",
+        answer: 3
+
+    },
+    {
+        question: "What type of Pokemon does Misty use?" ,
+        choice1: "Psychic",
+        choice2: "Rock",
+        choice3: "Dark",
+        choice4: "Water",
+        answer: 4
+
+    },
+    {
+        question: "Which of these Pokemon is not a starter Pokemon(Gen 1)?" ,
+        choice1: "Charmander",
+        choice2: "Pikachu",
+        choice3: "Bulbasaur",
+        choice4: "Squirtle",
+        answer: 2
+
+    },
+    {
+        question: "Fill in the blank: I want to be: _______" ,
+        choice1: "Stronger",
+        choice2: "The very best",
+        choice3: "The ultimate Pokemon catcher",
+        choice4: "Drunk",
+        answer: 2
+
+    },
+]
+
+// GamePlay
+
